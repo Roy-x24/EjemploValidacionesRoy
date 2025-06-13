fd6f0340818bce4173974a216976a5c6d686f160
Roy-x24 <roybarrerapty@gmail.com>
2025-06-13 02:38:33 -0500

    Se modifico

--- validate2.php
+++ validate2.php
@@ -1,13 +1,15 @@
 <?php
-include_once("ClaseValidacion.php");
+require_once("FormValidator.php");
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $validator = new FormValidator($_POST);
     $validator->setRequiredFields(['name', 'email', 'telefono']);
-    
     try {
         $validator->validate();
-        $name = htmlspecialchars($_POST['name']);
+        
+        // Capturamos el nombre limpio para mostrarlo
+        $name = htmlspecialchars(trim($_POST['name']));
+        
         echo "<div style='color:green;'>Formulario válido. ¡Gracias, $name!</div>";
     } catch (Exception $e) {
         echo "<div style='color:red;'>" . $e->getMessage() . "</div>";
