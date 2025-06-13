b029285f42ee0e0a160c2358c467475ae099b98c
Roy-x24 <roybarrerapty@gmail.com>
2025-06-13 02:36:26 -0500

    se modifico nombre y codigo y se elimino antiguo js.

--- FormValidator.php
+++ FormValidator.php
@@ -1,12 +1,13 @@
 <?php
-// https://mailtrap.io/blog/php-form-validation/#How-to-validate-a-form-in-PHP-using-script
 class FormValidator {
     private $data;
     private $requiredFields = [];
     private $errors = [];
+    private $captchaRequired = false;
 
-    public function __construct($postData) {
+    public function __construct($postData, $captchaRequired = false) {
         $this->data = $postData;
+        $this->captchaRequired = $captchaRequired;
     }
 
     public function setRequiredFields(array $fields) {
@@ -15,8 +16,12 @@ class FormValidator {
 
     public function validate() {
         $this->validateRequiredFields();
+        $this->validateNameFormat();
         $this->validateEmailFormat();
         $this->validateTelefonoFormat();
+        if ($this->captchaRequired) {
+            $this->validateCaptcha();
+        }
 
         if (!empty($this->errors)) {
             throw new Exception(implode("<br>", $this->errors));
@@ -31,6 +36,12 @@ class FormValidator {
         }
     }
 
+    private function validateNameFormat() {
+        if (!empty($this->data['name']) && !preg_match("/^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$/u", $this->data['name'])) {
+            $this->errors[] = "El nombre solo puede contener letras y espacios.";
+        }
+    }
+
     private function validateEmailFormat() {
         if (!empty($this->data['email']) && !filter_var($this->data['email'], FILTER_VALIDATE_EMAIL)) {
             $this->errors[] = "Formato de correo inválido.";
@@ -40,6 +51,12 @@ class FormValidator {
     private function validateTelefonoFormat() {
         if (!empty($this->data['telefono']) && !preg_match('/^\d{3}-\d{3}-\d{4}$/', $this->data['telefono'])) {
             $this->errors[] = "Teléfono inválido. Debe tener el formato 123-456-7890.";
+        }
+    }
+
+    private function validateCaptcha() {
+        if (empty($this->data['captcha']) || trim($this->data['captcha']) !== '7') {
+            $this->errors[] = "CAPTCHA incorrecto.";
         }
     }
 }
